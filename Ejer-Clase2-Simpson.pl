885919e75dd998674477fb93d1931fbd2c933dfe
Eduardo Goytendia <egoytendia@dialdata.com.ar>
2025-07-02 12:17:37 -0300

    ejercicio de los simpson, hecho en clase

---
+++ Ejer-Clase2-Simpson.pl
@@ -0,0 +1,30 @@
+% modelados de la consigna 
+padreDe(abe, abbie).
+padreDe(abe, homero).
+padreDe(abe, herbert).
+padreDe(clancy, marge).
+padreDe(clancy, patty).
+padreDe(clancy, selma).
+padreDe(homero, bart).
+padreDe(homero, hugo).
+padreDe(homero, lisa).
+padreDe(homero, maggie).
+
+madreDe(edwina, abbie).
+madreDe(mona, homero).
+madreDe(gaby, herbert).
+madreDe(jacqueline, marge).
+madreDe(jacqueline, patty).
+madreDe(jacqueline, selma).
+madreDe(marge, bart).
+madreDe(marge, hugo).
+madreDe(marge, lisa).
+madreDe(marge, maggie).
+madreDe(selma, ling).
+
+% funciones pedidas
+
+tieneHijos(padreDe) :-
+    
+
+tieneHijos(madreDe) :- 
